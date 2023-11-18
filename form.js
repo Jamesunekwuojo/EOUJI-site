10a599c8cb03ff86666e51b2624686331e68697d
jamesunekwuojo <unekwuojoabah4@gmail.com>
2023-11-18 19:18:21 +0100

    auto type features added

---
+++ form.js
@@ -0,0 +1,18 @@
+const Getext1 = document.getElementById("h1");
+const textoType1 = "Welcome to EOUJI Rentals Platform"
+const Getext2 = document.getElementById("textanime2");
+const textoType2 = "Please fill in the form below to continue"
+let index = 0;
+
+
+function autoType(){
+    if (index < textoType1.length && index < textoType2.length){
+        Getext1.innerHTML+= textoType1.charAt(index);
+        Getext2.innerHTML+= textoType2.charAt(index);
+        index++;
+        setTimeout(autoType, 100)
+
+    }
+}
+
+autoType();
